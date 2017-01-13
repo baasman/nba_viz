c8b66d4b2cc99d0d074ee4f39d40d771cd021aa6
Boudewijn Aasman <boudeyz@gmail.com>
2017-01-13 15:43:54 -0500

    added description table

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from constants import teams, var_view_map
 from nba_py.team import TeamGameLogs
 from helper_funcs import _color, _alpha
 
-from bokeh.plotting import figure
-from bokeh.layouts import layout, widgetbox, column, gridplot
+from bokeh.plotting import figure, show
+from bokeh.layouts import layout, widgetbox, column
 from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.models.widgets import Slider, Select, Button, DataTable
+from bokeh.models.widgets import Slider, Select, Button, DataTable, TableColumn
 from bokeh.io import curdoc
 
 team_options = list(teams.keys())
@@ -27,6 +27,16 @@ source_average = ColumnDataSource(data=dict(games=[], mean=[], std=[],
                                             _25percentile=[], median=[],
                                             _75percentile=[], max=[]))
 
+columns = [
+    TableColumn(field='games', title='GP'),
+    TableColumn(field="mean", title="Mean"),
+    TableColumn(field="std", title="Std"),
+    TableColumn(field="_25percentile", title="25th"),
+    TableColumn(field="median", title="Median"),
+    TableColumn(field="_75percentile", title="75th"),
+    TableColumn(field="max", title="max")
+]
+
 hover = HoverTool(tooltips=[
     ('Game', '$index'),
     ('Win', '@win'),
@@ -37,7 +47,9 @@ p = figure(plot_height=600, plot_width=700, title='',
            toolbar_location=None, tools=[hover])
 p.circle(x='x', y='y', source=source, size=7, color='color',
          line_color=None, fill_alpha='alpha')
-data_table = DataTable(source=source_average)
+data_table = DataTable(source=source_average, columns=columns,
+                       width=1000, height=300)
+
 
 def select_data():
     team = team_selection.value
@@ -66,17 +78,17 @@ def update():
     p.xaxis.axis_label = 'Game of season'
     p.yaxis.axis_label = y_axis.value
 
-    # averages = df[y_lab].describe()
-    # source_average.data = dict(
-    #     games=averages['count'],
-    #     mean=averages['mean'],
-    #     std=averages['std'],
-    #     min=averages['min'],
-    #     _25percentile=averages['25%'],
-    #     median=averages['50%'],
-    #     _75percentile=averages['75%'],
-    #     max=averages['max'],
-    # )
+    averages = df[y_lab].describe()
+    source_average.data = dict(
+        games=[round(averages['count'], 2)],
+        mean=[round(averages['mean'], 2)],
+        std=[round(averages['std'], 2)],
+        min=[round(averages['min'], 2)],
+        _25percentile=[round(averages['25%'], 2)],
+        median=[round(averages['50%'], 2)],
+        _75percentile=[round(averages['75%'], 2)],
+        max=[round(averages['max'], 2)],
+    )
 
     source.data = dict(
         x=df.index.values,
@@ -95,7 +107,7 @@ sizing_mode = 'fixed'
 
 inputs = widgetbox(team_selection, begin, end, y_axis, view_data,
                    sizing_mode=sizing_mode)
-_layout = layout([[inputs, p]], sizing_mode=sizing_mode)
+_layout = layout([[inputs, p], [data_table]], sizing_mode=sizing_mode)
 update()
 
 curdoc().add_root(_layout)
